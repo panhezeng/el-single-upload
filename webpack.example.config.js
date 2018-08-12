96037b6bbb84bc76e09656051e302ea4b981ae3a
潘何增 <panhezeng@xuetangx.com>
2018-08-12 11:13:46 +0800

    更新

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -65,11 +65,9 @@ const config = {
 }
 
 module.exports = (env, argv) => {
-
   if (argv.mode === 'production') {
     config.plugins.push(new BeforeRunWebpackPlugin({sed: 'vue'}))
   }
-
   return config
 }
 
