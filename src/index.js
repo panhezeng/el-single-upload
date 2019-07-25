5bda1837552964c23481c329afcaef9886c849e7
Pan Hezeng <panhezeng@gmail.com>
2019-07-25 16:02:24 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-export const ElSingleUpload = require('./ElSingleUpload.vue').default
+export const ElSingleUpload = require("./ElSingleUpload.vue").default;
 
-let Vue
+let Vue;
 
-function install (_Vue, options = (typeof window !== 'undefined' && window.ElSingleUploadOptions)) {
-
+function install(
+  _Vue,
+  options = typeof window !== "undefined" && window.ElSingleUploadOptions
+) {
   if (Vue) {
-    console.warn('[ElSingleUpload] already installed. Vue.use(ElSingleUpload) should be called only once.')
-    return
+    console.warn(
+      "[ElSingleUpload] already installed. Vue.use(ElSingleUpload) should be called only once."
+    );
+    return;
   }
 
-  Vue = _Vue
+  Vue = _Vue;
 
-  if (Object.prototype.toString.call(options) === '[object Object]') {
-    if (Object.prototype.toString.call(options.upload) === '[object Function]') {
+  if (Object.prototype.toString.call(options) === "[object Object]") {
+    if (
+      /^\[object [^F]*Function\]$/.test(
+        Object.prototype.toString.call(options.upload)
+      )
+    ) {
       Object.assign(ElSingleUpload.props.upload, {
         required: false,
         default: options.upload
-      })
+      });
     }
   }
 
-  Vue.component(ElSingleUpload.name, ElSingleUpload)
+  Vue.component(ElSingleUpload.name, ElSingleUpload);
 }
 
 /* istanbul ignore next */
-ElSingleUpload.install = install
+ElSingleUpload.install = install;
 
 // auto install in dist mode
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
-
-
