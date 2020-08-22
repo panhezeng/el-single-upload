b3e1e121ce806951b7051048dc8961aac26489bd
panhezeng <panhezeng@gmail.com>
2020-08-22 11:55:18 +0800

    cssloader4

--- webpack.config.js
+++ webpack.config.js
@@ -6,80 +6,89 @@ module.exports = {
     libraryTarget: "umd",
     umdNamedDefine: true,
     library: "ElSingleUpload",
-    libraryExport: "ElSingleUpload"
+    libraryExport: "ElSingleUpload",
   },
   externals: {
     "@panhezeng/utils/dist/check-upload.js": {
       commonjs: "@panhezeng/utils/dist/check-upload.js",
       commonjs2: "@panhezeng/utils/dist/check-upload.js",
       amd: "@panhezeng/utils/dist/check-upload.js",
-      root: "checkUpload"
+      root: "checkUpload",
     },
     "@panhezeng/utils/dist/get-object-item-by-path.js": {
       commonjs: "@panhezeng/utils/dist/get-object-item-by-path.js",
       commonjs2: "@panhezeng/utils/dist/get-object-item-by-path.js",
       amd: "@panhezeng/utils/dist/get-object-item-by-path.js",
-      root: "getObjectItemByPath"
+      root: "getObjectItemByPath",
     },
     "@panhezeng/el-popover-dialog": {
       commonjs: "@panhezeng/el-popover-dialog",
       commonjs2: "@panhezeng/el-popover-dialog",
       amd: "@panhezeng/el-popover-dialog",
-      root: "ElPopoverDialog"
+      root: "ElPopoverDialog",
     },
     vue: {
       commonjs: "vue",
       commonjs2: "vue",
       amd: "vue",
-      root: "Vue"
+      root: "Vue",
     },
     "element-ui": {
       commonjs: "element-ui",
       commonjs2: "element-ui",
       amd: "element-ui",
-      root: "ELEMENT"
-    }
+      root: "ELEMENT",
+    },
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: "vue-loader"
+        use: "vue-loader",
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
-        test: /\.(css|less)$/,
-        use: ["vue-style-loader", "css-loader", "less-loader"]
+        test: /\.((c|le)ss)$/i,
+        use: [
+          "vue-style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false,
+            },
+          },
+          "less-loader",
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/,
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: "img/[name].[hash:7].[ext]"
-        }
+          name: "img/[name].[hash:7].[ext]",
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: "media/[name].[hash:7].[ext]"
-        }
+          name: "media/[name].[hash:7].[ext]",
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: "fonts/[name].[hash:7].[ext]"
-        }
-      }
-    ]
+          name: "fonts/[name].[hash:7].[ext]",
+        },
+      },
+    ],
   },
-  plugins: [new VueLoaderPlugin()]
+  plugins: [new VueLoaderPlugin()],
 };
--- webpack.example.config.js
+++ webpack.example.config.js
@@ -7,56 +7,65 @@ const config = {
   output: {
     path: path.resolve(__dirname, "docs"),
     filename: "example.js",
-    chunkFilename: "[id].bundle.js"
+    chunkFilename: "[id].bundle.js",
   },
   externals: {
     vue: "Vue",
     "element-ui": "ELEMENT",
-    "@panhezeng/ucloud-ufile": "UCloudUFile"
+    "@panhezeng/ucloud-ufile": "UCloudUFile",
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: "vue-loader"
+        use: "vue-loader",
       },
       {
         test: /\.js$/,
         exclude: /node_modules|dist/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
-        test: /\.(css|less)$/,
-        use: ["vue-style-loader", "css-loader", "less-loader"]
+        test: /\.((c|le)ss)$/i,
+        use: [
+          "vue-style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false,
+            },
+          },
+          "less-loader",
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/,
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: "img/[name].[hash:7].[ext]"
-        }
+          name: "img/[name].[hash:7].[ext]",
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: "media/[name].[hash:7].[ext]"
-        }
+          name: "media/[name].[hash:7].[ext]",
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: "fonts/[name].[hash:7].[ext]"
-        }
-      }
-    ]
+          name: "fonts/[name].[hash:7].[ext]",
+        },
+      },
+    ],
   },
   plugins: [new VueLoaderPlugin()],
-  devServer: {}
+  devServer: {},
 };
 
 module.exports = (env, argv) => {
